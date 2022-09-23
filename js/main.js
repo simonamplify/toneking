7d41613d3a4daa16ed3c8c432af07c1f07c93ed4
Simon Frost <simon@amplifycreative.net>
2022-09-23 11:44:13 +0100

    header mobile styles

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
 (function($) {
-	/* Nested mobile menu */
-	function setup_collapsible_submenus() {
-		var $menu = $('#mobile_menu'),
-			top_level_link = '#mobile_menu .menu-item-has-children > a';
-		$menu.find('a').each(function() {
-			$(this).off('click'); 
-			if ( $(this).is(top_level_link) ) {
-				$(this).attr('href', '#');
-			}
-			if ( ! $(this).siblings('.sub-menu').length ) {
-				$(this).on('click', function(event) {
-					$(this).parents('.mobile_nav').trigger('click');
-				});
-			} else {
-				$(this).on('click', function(event) {
-					event.preventDefault();
-					$(this).parent().toggleClass('visible');
-				});
-			}
-		});
-	}
-	$(window).load(function() {
-		setTimeout(function() {
-			setup_collapsible_submenus();
-		}, 700);
-	});
+	// Nested mobile menu
+    function setup_collapsible_submenus() {
+        var $menu = $('.et_mobile_menu'),
+            top_level_link = '.et_mobile_menu .menu-item-has-children > a';
+        $menu.find('a').each(function() {
+            $(this).off('click'); 
+            if ( $(this).is(top_level_link) ) {
+                $(this).attr('href', '#');
+            }
+            if ( ! $(this).siblings('.sub-menu').length ) {
+                $(this).on('click', function(event) {
+                    $(this).parents('.mobile_nav').trigger('click');
+                });
+            } else {
+                $(this).on('click', function(event) {
+                    event.preventDefault();
+                    $(this).parent().toggleClass('visible');
+                });
+            }
+        });
+    }
+    $(window).load(function() {
+        setTimeout(function() {
+            setup_collapsible_submenus();
+        }, 700);
+    });
 	$(document).ready( function() {
 		// Menu Search
         $.fn.extend({
