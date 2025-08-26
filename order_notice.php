4b64558bfbb13c0a0e2f6a831799f35740a907b3
Simon-PC <simon@amplifycreative.net>
2025-08-26 10:25:40 +0100

    Updated order notice

--- order_notice.php
+++ order_notice.php
@@ -1,3 +1,4 @@
 <section id="order_notice">
+    <p><strong>US Smaller Item Shipping:</strong> Pedals and preamp pedals will ship FedEx 2 day at no cost for US customers.</p>
     <p><strong>International Orders (Outside Continental USA):</strong> This order may be subject to import duties, taxes, or fees in the destination country. These costs are not included in your order and will be billed to you by the transporter or customs authority during the customs clearance process or upon delivery.</p>
 </section>
