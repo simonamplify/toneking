6821f72fd089da79c97395690cd3fba4ef7242df
Simon-PC <simon@amplifycreative.net>
2025-12-03 10:02:17 +0000

    refactored the function enqueue scripts

--- functions.php
+++ functions.php
@@ -1,28 +1,35 @@
 <?php
+// Enqueue parent and child theme styles and scripts
 function custom_theme_scripts() {
-
-    $parent_style = 'Divi';
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style )
+    $parent_handle = 'Divi';
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_directory_uri() . '/css/stylesheet.css',
+        array( $parent_handle ),
+        filemtime( get_stylesheet_directory() . '/css/stylesheet.css' )
     );
-    wp_enqueue_script( 'list-js', get_bloginfo( 'stylesheet_directory' ) . '/js/list.js', null, null, true);
-    wp_enqueue_script( 'main-js', get_bloginfo( 'stylesheet_directory' ) . '/js/main.js', array('list-js'), null, true);
+    wp_enqueue_script(
+        'list-js',
+        get_stylesheet_directory_uri() . '/js/lib/list.min.js',
+        null,
+        null,
+        true
+    );
+    wp_enqueue_script(
+        'main-js',
+        get_stylesheet_directory_uri() . '/js/main.min.js',
+        array('list-js'),
+        filemtime( get_stylesheet_directory() . '/js/main.min.js' ),
+        true
+    );
 }
-add_action( 'wp_enqueue_scripts', 'custom_theme_scripts' );
+add_action( 'wp_enqueue_scripts', 'custom_theme_scripts', 11 ); // 11 to ensure it runs after the parent theme's scripts
 // Enqueue login styles
 function my_logincustomCSSfile() {
     wp_enqueue_style('login-styles', get_stylesheet_directory_uri() . '/css/login_stylesheet.css');
 }
 add_action('login_enqueue_scripts', 'my_logincustomCSSfile');
-// Automatically Update Wordpress
-add_filter( 'auto_update_core', '__return_true' );
-// Automatically Update Plugins
-//add_filter( 'auto_update_plugin', '__return_true' );
-// Automatically Update Theme
-//add_filter( 'auto_update_theme', '__return_true' );
+
 // Change login logo url and title text
 function my_loginURL() {
     return '/';
